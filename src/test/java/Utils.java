0bba68ed5fe210e500230c43308a9065c3bf3e29
19burak03 <40755627+19burak03@users.noreply.github.com>
2021-07-26 17:17:57 +0300

    Update Utils.java

--- src/test/java/Utils.java
+++ src/test/java/Utils.java
@@ -1,5 +1,5 @@
 
 public class Utils {
-    public static final String BASE_URL = "http://localhost:8080";
+    public static final String BASE_URL = "http://localhost:8080/clusters/dev";
     public static final String CHROME_DRIVER_LOCATION = "/usr/local/bin/chromedriver";
 }
