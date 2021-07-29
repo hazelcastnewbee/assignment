7e438431ddb7418726c4b43e5a46a9b7791287e6
19burak03 <19burak03@gmail.com>
2021-07-29 22:54:26 +0300

    hazelcast code chall

--- src/test/java/Utils.java
+++
@@ -1,5 +0,0 @@
-
-public class Utils {
-    public static final String BASE_URL = "http://localhost:8080/clusters/dev";
-    public static final String CHROME_DRIVER_LOCATION = "/usr/local/bin/chromedriver";
-}
