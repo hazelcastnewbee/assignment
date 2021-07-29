1d432b313035cd77ffe3d794d6e564c744deeabf
19burak03 <40755627+19burak03@users.noreply.github.com>
2021-07-29 14:01:22 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -5,8 +5,11 @@ import java.util.*;
 public class Main {
 
     public static void main(String[] args) {
+       System.out.println(args[0] + "  " + args[1]);
+       String frk = args[0];
+       Integer brk = Integer.valueOf(args[1]);
        HazelcastInstance hazelcastInstance = Hazelcast.newHazelcastInstance();
-       Map<String, String> map = hazelcastInstance.getMap("default");
+       Map<String, String> map = hazelcastInstance.getMap("frk");
        for (int i = 0; i < 100; i++) {
            map.put("key" + i, "value" + i);
        }
